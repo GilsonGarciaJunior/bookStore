a756955af9d3850f64584f7154a3acfdd145562a
GilsonGarciaJunior <juniorrodrigues082006@gmail.com>
2026-08-29 23:38:41 -0300

    test: webhook deploy

--- bookstore/views.py
+++ bookstore/views.py
@@ -14,7 +14,6 @@ def update(request):
         '''
         repo = git.Repo('/home/GilsonGarciaJunior/bookStore')
         origin = repo.remotes.origin
-
         origin.pull('main')
         return HttpResponse("Updated code on PythonAnywhere")
     else:
